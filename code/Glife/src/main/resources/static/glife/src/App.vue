3d04010736277d22e70ec7ee5d7e8ff95bbe0aae
YUXIN SUN <2029536227@qq.com>
2024-06-12 22:35:47 +0100

    Create a simple chatWindow

--- code/Glife/src/main/resources/static/glife/src/App.vue
+++ code/Glife/src/main/resources/static/glife/src/App.vue
@@ -1,47 +1,31 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <ChatWindow />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import ChatWindow from './components/ChatWindow.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ChatWindow
+  }
+};
+</script>
+
+<style>
+html, body, #app {
+  height: 100%;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#app {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
 }
 </style>
