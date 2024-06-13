0ba5fb43cdc9949bcd46bb5be13934f92037fef3
ada <liangjingtimes@gmail.com>
2024-06-13 13:50:40 +0100

    try authenticator but resource not found

--- code/Glife/src/main/java/com/example/glife/service/impl/AssistantServiceImp.java
+++ code/Glife/src/main/java/com/example/glife/service/impl/AssistantServiceImp.java
@@ -2,7 +2,9 @@ package com.example.glife.service.impl;
 
 import com.example.glife.config.AssistantConfig;
 import com.example.glife.service.AssistantService;
+import com.ibm.cloud.sdk.core.http.HttpConfigOptions;
 import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.watson.assistant.v1.model.MessageResponse;
 import com.ibm.watson.assistant.v2.Assistant;
 import com.ibm.watson.assistant.v2.model.*;
 
@@ -11,15 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-
 @Service
 @Scope("prototype")
 @Slf4j
-public class AssistantServiceImp implements AssistantService{
+public class AssistantServiceImp implements AssistantService {
 
     private Assistant assistant;
     private String sessionId;
+    private String assistantId;
 
     @Autowired
     private AssistantConfig assistantConfig;
@@ -29,18 +30,30 @@ public class AssistantServiceImp implements AssistantService{
         IamAuthenticator authenticator = new IamAuthenticator(assistantConfig.getApikey());
         assistant = new Assistant(assistantConfig.getVersion(), authenticator);
         assistant.setServiceUrl(assistantConfig.getServiceUrl());
+        assistantId = assistantConfig.getAssistantId();
     }
 
     public Assistant getAssistant() {
         return assistant;
     }
 
-    public void createSession(){
+    public void setAssistant(Assistant assistant) {
+        this.assistant = assistant;
+    }
+
+    public void createSession() {
         CreateSessionOptions options = new CreateSessionOptions.Builder(assistantConfig.getAssistantId()).build();
         SessionResponse response = assistant.createSession(options).execute().getResult();
         sessionId = response.getSessionId();
-        log.info("Session ID: {}" ,sessionId);
+        log.info("Session ID: {}", sessionId);
+    }
 
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public String getAssistantId() {
+        return assistantId;
     }
 
     public void closeSession() {
@@ -51,15 +64,43 @@ public class AssistantServiceImp implements AssistantService{
         }
     }
 
-    public MessageResponse sendMessage(String inputMessage) {
+    public StatefulMessageResponse sendMessage(String inputMessage) {
         MessageInput input = new MessageInput.Builder()
+                .messageType("text")
                 .text(inputMessage)
                 .build();
 
-        MessageOptions options = new MessageOptions.Builder(assistantConfig.getAssistantId(),sessionId)
+        MessageOptions options = new MessageOptions.Builder(assistantId, sessionId)
                 .input(input)
                 .build();
 
         return assistant.message(options).execute().getResult();
+    }
+
+
+    public static void main(String[] args) {
+        IamAuthenticator authenticator = new IamAuthenticator("S4m5c3m1vdnSy0WKlEHwbTndaAxqZBKcKiDDTAIKbuO5");
+        Assistant assistant = new Assistant("2023-06-15", authenticator);
+        assistant.setServiceUrl("https://api.eu-gb.assistant.watson.cloud.ibm.com");
+
+        CreateSessionOptions createSessionOptions = new CreateSessionOptions.Builder("d5404082-d252-4ecc-9df0-ece0170f0970").build();
+        SessionResponse sessionResponse = assistant.createSession(createSessionOptions).execute().getResult();
+        String sessionId = sessionResponse.getSessionId();
+        System.out.print(sessionId);
+
+
+//        service.createSession();
+////        String sessionId = service.getSessionId();
+//        MessageInput input = new MessageInput.Builder()
+//                .messageType("text")
+//                .text("Hello")
+//                .build();
+//
+//        MessageOptions options = new MessageOptions.Builder("d5404082-d252-4ecc-9df0-ece0170f0970",sessionId)
+//                .input(input)
+//                .build();
+//
+//        StatefulMessageResponse response = assistant.message(options).execute().getResult();
+//        System.out.println(response);
     }
 }
---
+++ code/Glife/src/test/java/com/example/glife/AssistantAPITest.java
@@ -0,0 +1,67 @@
+package com.example.glife;
+
+import com.example.glife.config.AssistantConfig;
+import com.example.glife.service.AssistantService;
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.cloud.sdk.core.service.exception.RequestTooLargeException;
+import com.ibm.cloud.sdk.core.service.exception.ServiceResponseException;
+import com.ibm.watson.assistant.v1.model.MessageResponse;
+import com.ibm.watson.assistant.v2.Assistant;
+import com.ibm.watson.assistant.v2.model.MessageInput;
+import com.ibm.watson.assistant.v2.model.MessageOptions;
+import com.ibm.watson.assistant.v2.model.RuntimeResponseGeneric;
+import com.ibm.watson.assistant.v2.model.StatefulMessageResponse;
+import org.apache.ibatis.javassist.NotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class AssistantAPITest {
+    @Autowired
+    private AssistantService assistantService;
+
+    private String sessionId;
+
+    @BeforeEach
+    void setUp() {
+        assistantService.initializeAssistant();
+        assistantService.createSession();
+        sessionId = assistantService.getSessionId();
+    }
+
+    @Test
+    void getVersion(){
+
+
+    }
+    @Test
+    void helloAssistant() {
+
+        try {
+            MessageInput input = new MessageInput.Builder()
+                    .messageType("text")
+                    .text("what do you do")
+                    .build();
+
+            MessageOptions options = new MessageOptions.Builder(assistantService.getAssistantId(), sessionId)
+                    .input(input)
+                    .build();
+
+            StatefulMessageResponse response = assistantService.getAssistant().message(options).execute().getResult();
+
+            System.out.println(response.toString());
+            // Invoke a method
+        } catch (RequestTooLargeException e) {
+            // Handle Request Too Large (413) exception
+        } catch (ServiceResponseException e) {
+            // Base class for all exceptions caused by error responses from the service
+            System.out.println("Service returned status code "
+                    + e.getStatusCode() + ": " + e.getMessage());
+        }
+
+    }
+}
