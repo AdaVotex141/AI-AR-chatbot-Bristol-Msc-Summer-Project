8ff1b2eb2a8aa8acbefa259333040927207b8566
MeiloMuI <hxy244198079@outlook.com>
2024-06-18 22:51:09 +0100

    change css of artree

--- code/Glife/src/main/resources/static/glife/src/App.vue
+++ code/Glife/src/main/resources/static/glife/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h2>Glife</h2>
+    <h2 class="title">Glife</h2>
     <div class="navigate">
       <RouterLink to="/">Chat</RouterLink>
       <RouterLink to="/dayroutine">DayRoutine</RouterLink>
@@ -18,16 +18,27 @@
 </script>
 
 <style>
-html, body, #app {
-  height: 100%;
-  margin: 0;
-  display: flex;
-  flex-direction: column;
-}
+  .title{
+      background-color:#438944;
+      color: white;
+      text-align: center;
+      padding: 1rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+  }
 
-#app {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-}
+  html, body, #app {
+    height: 100%;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+  }
+
+  #app {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+
 </style>
--- code/Glife/src/main/resources/static/glife/src/components/ArTree.vue
+++ code/Glife/src/main/resources/static/glife/src/components/ArTree.vue
@@ -1,5 +1,5 @@
 <template>
-<div>ArTree content here</div>
+<div class="main-content">ArTree content here</div>
 </template>
 
 <script lang='ts' setup>
@@ -7,4 +7,12 @@
 </script>
 
 <style scoped>
+    .main-content{
+        margin: 0 auto;
+        margin-top: 30px;
+        border-radius: 10px;
+        width: 90%;
+        height: 400px;
+        border: 1px solid;
+    }
 </style>
