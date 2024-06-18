c70936ca5eeed132a8e10bbef37bd56bc09f0e1d
Ada <liangjingtimes@gmail.com>
2024-06-18 14:16:50 +0100

    current working on dialoge, missing skill

--- code/Glife/src/main/java/com/example/glife/service/AssistantService.java
+++ code/Glife/src/main/java/com/example/glife/service/AssistantService.java
@@ -5,12 +5,9 @@ import com.ibm.watson.assistant.v2.model.StatefulMessageResponse;
 
 public interface AssistantService {
     void initializeAssistant();
-    void setAssistant(Assistant assistant);
-
     Assistant getAssistant();
     void createSession();
     void closeSession();
     String getSessionId();
-    String getAssistantId();
     StatefulMessageResponse sendMessage(String inputMessage);
 }
