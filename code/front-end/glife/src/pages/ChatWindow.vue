c22725033dbc0ae87cc610eeb2ad6d0e4d426601
“MeiloMuI” <hxy244198079@outlook.com>
2024-07-02 11:23:23 +0100

    add handle data function to show the feedback on the screen

--- code/front-end/glife/src/pages/ChatWindow.vue
+++ code/front-end/glife/src/pages/ChatWindow.vue
@@ -21,21 +21,37 @@ async function handleSendMessage(message){
   // Show the message on the window
   messages.value.push({ text: message, sender: 'user' });
 
-  console.log(message, 'sbsbs')
   // Send api request to the backend
   try{
-    
     const response = await axios.post('/api/assistant/input', {
       inputMessage: message
     })
-    console.log(response.data)
-
+    const data = response.data
+    console.log(data.data)
+    if(String(data.code) === '1'){
+      handleResponseData(data.data)  
+    } else {
+      messages.value.push({text: 'Server did not give response, please try again.', sender: 'bot'})
+    }
   } catch (error){
-    console.error(error)
+    messages.value.push({text: 'Bad things happened, please try again.', sender: 'bot'})
   }
-  
-  messages.value.push({ text: 'This is a bot response', sender: 'bot' });
 };
+
+function handleResponseData(data){
+  if(data && Array.isArray(data.responseSectionList)){
+    data.responseSectionList.forEach((item) => {
+      if(item.responseType === 'text'){
+        messages.value.push({text: item.text, sender: 'bot'})
+      } else {
+        let result = item.responseType + '---' + item.text
+        messages.value.push({text: result, sender: 'bot'})
+      }
+    })
+  } else {
+    messages.value.push({text: 'wtf idk', sender: 'bot'})
+  }
+}
 
 </script>
 
