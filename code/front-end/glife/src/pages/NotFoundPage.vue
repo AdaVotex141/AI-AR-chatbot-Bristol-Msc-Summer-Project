85d8aaa2a6f5af1531cf4fa1ef20147dbea52c50
YUXIN SUN <2029536227@qq.com>
2024-07-04 12:12:22 +0100

    Update the 404 page

--- code/front-end/glife/src/pages/NotFoundPage.vue
+++ code/front-end/glife/src/pages/NotFoundPage.vue
@@ -1,6 +1,3 @@
-<script setup lang="ts">
-
-</script>
 
 <template>
     <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
@@ -9,13 +6,29 @@
         <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
         <p class="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
-          <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</a>
-          <a href="#" class="text-sm font-semibold text-gray-900">Contact support <span aria-hidden="true">&rarr;</span></a>
+          <button class="rounded-md bg-green-200 px-3.5 py-2.5 text-sm font-semibold
+          text-black shadow-sm hover:bg-green-300 focus-visible:outline focus-visible:outline-2
+          focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          @click="navigateTo('logout')">
+            Go back home</button>
+          <button class="text-sm font-semibold text-gray-900">
+            Contact support<span aria-hidden="true">&rarr;</span>
+          </button>
         </div>
       </div>
     </main>
 
 </template>
+
+<script setup lang="ts">
+import router from "@/router";
+
+function navigateTo(routeName: string) {
+  router.push({
+    name: routeName
+  });
+}
+</script>
 
 <style scoped>
 .text-base{
