0ed73d28f9763e2a1b52948eac900407577e0ada
YUXIN SUN <2029536227@qq.com>
2024-07-08 13:15:16 +0100

    Add the button click to change the page

--- code/front-end/glife/src/components/MobileMenu.vue
+++ code/front-end/glife/src/components/MobileMenu.vue
@@ -1,4 +1,19 @@
 <script setup lang="ts">
+import router from "@/router";
+import { useRoute } from 'vue-router';
+import { onMounted, computed } from 'vue';
+
+// Watch the current router
+const route = useRoute();
+const isActive = (routeName: string) => {
+  return computed(() => route.name === routeName);
+};
+// navigate to the needed place
+function navigateTo(routeName: string) {
+  router.push({
+    name: routeName
+  });
+}
 
 </script>
 
@@ -6,7 +21,7 @@
   <div class="fixed bottom-0 z-50 w-full h-16 -translate-x-1/2 bg-white border-t border-gray-200 left-1/2 dark:bg-gray-700 dark:border-gray-600">
     <div class="grid  h-full max-w-lg grid-cols-5 mx-auto">
 <!--      Chat bot button style-->
-      <button data-tooltip-target="tooltip-chat" type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+      <button type="button" :class="{'active-button': isActive('chatwindow').value}" data-tooltip-target="tooltip-chat"  class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group" @click="navigateTo('chatwindow')">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 22" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-500 dark:group-hover:text-blue-500" aria-hidden="true">
           <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12
           m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025
@@ -19,7 +34,7 @@
         <div class="tooltip-arrow" data-popper-arrow></div>
       </div>
 <!--      Day routine button style-->
-      <button data-tooltip-target="tooltip-todolist" type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+      <button :class="{'active-button': isActive('dayroutine').value}" data-tooltip-target="tooltip-todolist" type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group" @click="navigateTo('dayroutine')">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 22" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-500 dark:group-hover:text-blue-500" aria-hidden="true">
           <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08
           m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15
@@ -34,7 +49,7 @@
         <div class="tooltip-arrow" data-popper-arrow></div>
       </div>
 <!--      Back to home button style-->
-      <button data-tooltip-target="tooltip-home" type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-600 group">
+      <button :class="{'active-button': isActive('startpage').value}" data-tooltip-target="tooltip-home" type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-600 group" @click="navigateTo('startpage')">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 22" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-500 dark:group-hover:text-blue-500" aria-hidden="true">
           <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
         </svg>
@@ -45,7 +60,7 @@
         <div class="tooltip-arrow" data-popper-arrow></div>
       </div>
 <!--      ArTree button style-->
-      <button data-tooltip-target="tooltip-ARtree" type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+      <button :class="{'active-button': isActive('artree').value}" data-tooltip-target="tooltip-ARtree" type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group" @click="navigateTo('artree')">
         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" fill="none" viewBox="0 0 22 22" stroke="currentColor" stroke-width="1.5">
           <path stroke-linecap="round" stroke-linejoin="round" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M12 13l-3 -3M12 13l3 -3M12 13v10" />
         </svg>
@@ -55,7 +70,7 @@
         Settings
         <div class="tooltip-arrow" data-popper-arrow></div>
       </div>
-      <button data-tooltip-target="tooltip-profile" type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+      <button :class="{'active-button': isActive('setting').value}" data-tooltip-target="tooltip-profile" type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group" @click="navigateTo('setting')">
         <svg class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
           <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
         </svg>
