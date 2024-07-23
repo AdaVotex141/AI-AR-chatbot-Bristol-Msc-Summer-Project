ad021685944e83dbd4c2b23c02b4275edd2f28de
“MeiloMuI” <hxy244198079@outlook.com>
2024-07-23 13:19:18 +0100

    finish init tasks function

--- code/front-end/glife/src/stores/task.ts
+++ code/front-end/glife/src/stores/task.ts
@@ -29,30 +29,25 @@ export const useTaskStore = defineStore('task',()=>{
         try{
             const response = await axios.post('/api/admin/randomTask/init')
             console.log(response.data.data)
-            // if(String(response.data.code) === '1'){
-            //     //Get the data from response
-            //     const data: {id: number; username: string; permission:number}[] = response.data.data
-            //     //Map the data to todos and Sort them with id ascendingly
-            //     tasks.value = data.map(item => ({
-            //         id: item.id,
-            //         username: item.username,
-            //         permission: item.permission
-            //     }))
-            //     .sort((a, b) => {
-            //         if(a.permission !== b.permission){
-            //             return b.permission - a.permission
-            //         } else {
-            //             return a.username.localeCompare(b.username)
-            //         }
-            //     })
-            // } else {
-            //     alert('Backend give a code 0?') // TODO: need to be deleted after ensuring the fault
-            //     router.push({name:'notfound'});
-            // }
+            if(String(response.data.code) === '1'){
+                //Get the data from response
+                const data: {
+                    id:number;
+                    title:string;
+                    description:string;
+                    creator:string;
+                    create_time:string;
+                    schedule:number}[] = response.data.data
+                //Save data in the tasks
+                tasks.value = data
+            } else {
+                alert('Backend give a code 0?') // TODO: need to be deleted after ensuring the fault
+                router.push({name:'notfound'});
+            }
         } catch (error) {
             console.error("Error happened during initializing the tasks")
             alert("Error happened during initializing the tasks")
-            // router.push({name:'notfound'});
+            router.push({name:'notfound'});
         }
     }
 
