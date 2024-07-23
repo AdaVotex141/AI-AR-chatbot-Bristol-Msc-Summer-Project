d8f9aa94535366c0b393fad4a0a8e1f6c0f4be44
“MeiloMuI” <hxy244198079@outlook.com>
2024-07-23 12:42:29 +0100

    adjust logout function in adminpage

--- code/front-end/glife/src/components/TaskForm.vue
+++ code/front-end/glife/src/components/TaskForm.vue
@@ -22,7 +22,7 @@
         </el-form-item>
         <el-form-item>
             <el-button type="primary" class="center text-white inline-flex w-full justify-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" 
-            @click="addNewTask(ruleFormRef)">Add</el-button>
+            @click="addNewTask(ruleFormRef)">Publish</el-button>
         </el-form-item>
     </el-form>
   </template>
