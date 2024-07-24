34c3becdc631f70b1e3284319bf7c0372cb6a0b5
ada <liangjingtimes@gmail.com>
2024-07-24 18:08:07 +0100

    add Statistics

--- code/back-end/Glife/src/main/java/com/example/glife/mapper/RoutineMapper.java
+++ code/back-end/Glife/src/main/java/com/example/glife/mapper/RoutineMapper.java
@@ -3,6 +3,8 @@ package com.example.glife.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.glife.entity.Routine;
 import com.example.glife.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface RoutineMapper  extends BaseMapper<Routine> {
 }
