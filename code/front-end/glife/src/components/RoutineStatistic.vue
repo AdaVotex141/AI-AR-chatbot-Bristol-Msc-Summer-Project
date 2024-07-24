8788ceeeb27d57aa9ba2a0250a447a1dcf06f6c3
“MeiloMuI” <hxy244198079@outlook.com>
2024-07-24 22:22:00 +0100

    add statistic part in the dashboard

--- code/front-end/glife/src/components/RoutineStatistic.vue
+++ code/front-end/glife/src/components/RoutineStatistic.vue
@@ -1,30 +1,116 @@
 <template>
-    <div class="demo-progress">
-      <el-progress :text-inside="true" :stroke-width="26" :percentage="70" />
-      <el-progress
-        :text-inside="true"
-        :stroke-width="24"
-        :percentage="100"
-        status="success"
-      />
-      <el-progress
-        :text-inside="true"
-        :stroke-width="22"
-        :percentage="80"
-        status="warning"
-      />
-      <el-progress
-        :text-inside="true"
-        :stroke-width="20"
-        :percentage="50"
-        status="exception"
-      />
-    </div>
-  </template>
+    <el-container>
+        <el-header>Your Routine:</el-header>
+        <el-main>
+            <div class="progress-container">
+                <div class="progress-item">
+                    <el-progress :text-inside="true" :stroke-width="24" :percentage='dailyStat' >
+                    <span>{{dailyStat}}%</span>
+                    </el-progress>
+                    <el-button text>Daily</el-button>
+                </div>
+                <div class="progress-item">
+                    <el-progress
+                    :text-inside="true"
+                    :stroke-width="24"
+                    :percentage="weeklyStat"
+                    status="success"
+                    >
+                    <span>{{weeklyStat}}%</span>
+                    </el-progress>
+                    <el-button text>Weekly</el-button>
+                </div>
+                <div class="progress-item">
+                    <el-progress
+                    :text-inside="true"
+                    :stroke-width="24"
+                    :percentage="monthlyStat"
+                    status="warning"
+                    >
+                    <span>{{monthlyStat}}%</span>
+                    </el-progress>
+                    <el-button text>Monthly</el-button>
+                </div>
+            </div>
+        </el-main>
+    </el-container>
+</template>
+
+<script setup lang="ts">
+import router from '@/router';
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+const dailyStat = ref(0)
+const weeklyStat = ref(0)
+const monthlyStat = ref(0)
+
+async function getRoutineStatistic(){
+    try{
+        const response = await axios.get('/api/stat')
+        if(String(response.data.code) === '1'){
+            const data = response.data.data
+            dailyStat.value = data.daily
+            weeklyStat.value = data.weekly
+            monthlyStat.value = data.monthly
+        }
+    } catch (error){
+        console.error(error)
+        router.replace({name: 'notfound'})
+    }
+}
+
+onMounted(()=>{
+    getRoutineStatistic()
+})
+
+</script>
   
-  <style scoped>
-  .demo-progress .el-progress--line {
-    margin-bottom: 15px;
-    max-width: 600px;
+<style scoped>
+.el-header{
+    background-color: #9cb470;
+    padding: 1.5rem;
+    margin-bottom: 0.5rem;
+    font-size: 1.5rem;
+    color: whitesmoke;
+    font-weight: bold;
+    font-family: 'Cooper Black',sans-serif;
+    border-bottom: 1px solid #e0e0e0;
+    border-radius: 0.8rem;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: background-color 0.3s ease, box-shadow 0.3s ease;
+    top: 5%;
   }
-  </style>
+
+span{
+    color: black;
+}
+
+.el-container{
+    background-color: bisque;
+}
+
+.progress-container {
+    display: flex;
+    flex-direction: column; 
+    gap: 20px; 
+}
+
+.progress-item {
+    display: flex;
+    align-items: center; 
+    gap: 10px; 
+}
+
+.el-progress {
+    flex: 1; 
+    min-width: 50px;
+}
+
+.el-button {
+    white-space: nowrap; 
+}
+</style>
--- code/front-end/glife/src/pages/DashBoard.vue
+++ code/front-end/glife/src/pages/DashBoard.vue
@@ -1,13 +1,27 @@
 <template>
-    <div>
+    <div class="container">
         <UserRandomTask />
+        <RoutineStatistic class="routine-part" />
     </div>
 </template>
 <script setup lang="ts">
+import RoutineStatistic from '@/components/RoutineStatistic.vue';
 import UserRandomTask from '@/components/UserRandomTask.vue';
 
 
 </script>
 <style scoped>
-    
+.container {
+    display: flex;
+    flex-direction: column; 
+    width: 100%;
+    height: 100%;
+}
+
+.routine-part {
+    display: flex;
+    height: 40vh; 
+    width: 80vw; 
+    border: 1px solid #000; 
+}
 </style>
