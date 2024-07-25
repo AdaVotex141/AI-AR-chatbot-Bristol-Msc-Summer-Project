7fa3fc3fa40c3442360a7f86c3bc39d94543ec1d
“MeiloMuI” <hxy244198079@outlook.com>
2024-07-25 21:46:59 +0100

    connect treesum with backend

--- code/front-end/glife/src/components/TreeStatistic.vue
+++ code/front-end/glife/src/components/TreeStatistic.vue
@@ -4,9 +4,35 @@
     </div>
 </template>
 <script setup lang='ts'>
-import { ref } from 'vue';
+import router from '@/router';
+import axios from 'axios';
+import { computed, onMounted, ref } from 'vue';
 
-const message = ref('You have already planted 100 trees.')
+const message = computed(()=>{
+    return 'You have already planted ' + treeSum.value + " trees."
+})
+const treeSum = ref(0)
+
+onMounted(()=> {
+    getTreeInfo()
+})
+
+async function getTreeInfo(){
+    try{
+        const response = await axios.get('/api/ARTree/getTreeSum')
+        console.log(response)
+        if(String(response.data.code) !== '1'){
+            console.error('cannot get treesum in the treestatistic.')
+        } else {
+            treeSum.value = response.data.data
+        }
+    } catch (error){
+        console.error(error)
+        router.replace({
+            name: 'notfound'
+        })
+    }
+}
 </script>
 <style scoped>
     
