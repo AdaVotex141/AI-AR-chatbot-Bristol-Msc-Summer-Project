217fc1baa49796ecf80ea9e6ff2776425705a62c
XinyueFeng99 <fxy13030305896@163.com>
2024-07-26 21:42:45 +0800

    return description of badges

--- code/back-end/Glife/src/main/java/com/example/glife/service/UserBadgeService.java
+++ code/back-end/Glife/src/main/java/com/example/glife/service/UserBadgeService.java
@@ -1,12 +1,13 @@
 package com.example.glife.service;
 
 import com.example.glife.common.R;
+import com.example.glife.entity.Badge;
 import com.example.glife.entity.UserBadge;
 
 import java.util.List;
 
 public interface UserBadgeService {
-    R<List<Long>> getUserBadgesByUserId(Long userId);
+    R<List<Badge>> getUserBadgesByUserId(Long userId);
 //    R<UserBadge> addUserBadge(UserBadge userBadge);
     R<String> deleteUserBadge(Long userId, Long badgeId);
     void checkAndAwardFirstTreePlanterBadge(Long userId);
